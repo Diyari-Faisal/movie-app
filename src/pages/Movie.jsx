8b363028e6ce67f74a2efa9e91a028ac81e95aaf
DemonPlayer96 <diyai2006@gmail.com>
2025-04-03 19:32:44 +0300

    design updates

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -4,7 +4,7 @@ import "../css/Movie.css";
 
 function MoviePage() {
     const location = useLocation();
-    const movie = location.state?.movie;
+    const movie = location.state.movie;
 
     if (!movie) {
         return <p>Movie not found</p>;
@@ -14,14 +14,19 @@ function MoviePage() {
     const movie_url = `${BASE_URL}?tmdb=${movie.id}`;
 
     return (
-        <div className="movie-window">
-            {movie_url ? (
-                <iframe className="movie-window" src={movie_url} allowFullScreen></iframe>
-            ) : (
-                <p>Loading movie or unable to fetch movie details...</p>
-            )}
-            <p>{movie.title}</p>
-            <a href={movie_url} target="_blank">if the player doesn't work click here</a>
+        <div className="movie-page">
+            <div className="movie-window">
+                {movie_url ? (
+                    <iframe className="movie-window" src={movie_url} allowFullScreen></iframe>
+                ) : (
+                    <p>Loading movie or unable to fetch movie details...</p>
+                )}
+            </div>
+
+            <div className="movie-title">
+                <p>{movie.title}</p>
+                <a href={movie_url} target="_blank">if the player doesn't work click here</a>
+            </div>
         </div>
     );
 }
